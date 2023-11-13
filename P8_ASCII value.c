959cb0cd3d8d5b9787d5419079f4c58c11810feb
shreeja00 <144765155+shreeja00@users.noreply.github.com>
2023-11-13 14:39:54 +0530

    Add files via upload

---
+++ P8_ASCII value.c
@@ -0,0 +1,9 @@
+// Write a program in to print the ASCII value of a character
+#include<stdio.h>
+int main(){
+    char c;
+    printf("ENTER THE CHARACTER: ");
+    scanf("%c", &c);
+    printf("THE ASCII VALUE OF %c is %d\n", c, c);
+
+}
